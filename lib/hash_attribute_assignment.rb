5c384106c66ee876e8d247be5c867349fc2cedee
Corey Alexander <corey@betterment.com>
2017-05-21 17:13:00 -0400

    Commit rubo violation to test circle

--- lib/hash_attribute_assignment.rb
+++ lib/hash_attribute_assignment.rb
@@ -3,7 +3,7 @@ module HashAttributeAssignment
     HashValidation = Struct.new(:proc, :message)
 
     def validate_hash(proc, options = { message: 'Hash failed validation' })
-      raise ArgumentError('First arg must be a call-able') unless proc.respond_to? :call
+      raise ArgumentError("First arg must be a call-able") unless proc.respond_to? :call
       class_variable_set(:@@hash_validations, hash_validations + [HashValidation.new(proc, options[:message])])
     end
 
