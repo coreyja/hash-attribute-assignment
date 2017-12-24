5b759b38d4956741bdc37b4a041c7d34e40ec173
Corey Alexander <coreyja@gmail.com>
2017-12-23 20:41:23 -0500

    show counts and add specs to break on purpose

--- spec/hash_attribute_assignment_spec.rb
+++ spec/hash_attribute_assignment_spec.rb
@@ -130,4 +130,14 @@ RSpec.describe HashAttributeAssignment do
       end
     end
   end
+
+  context 'group of tests to break on purpose' do
+    it 'breaks' do
+      expect(1).to eq 2
+    end
+
+    it 'works' do
+      expect(1).to eq 1
+    end
+  end
 end
