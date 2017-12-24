aefc26e971d36c6aba07f92d14a8fabaca0775a0
Corey Alexander <coreyja@gmail.com>
2017-12-24 17:37:28 -0500

    Remove line from spec helper. I want this to be a circleci only solution

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,6 +93,4 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-
-  config.example_status_persistence_file_path = '.rspec_failed_examples'
 end
